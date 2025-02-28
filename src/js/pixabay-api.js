018b58ceea05094376dc194e35a69406edb9800c
Veronika0136 <Artemova331@gmail.com>
2025-03-01 00:45:12 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
-export function getAllImages(massege) {
+export async function getAllImages(massege,page,perPage) {
   const baseURL = 'https://pixabay.com/api/';
 
   const params = {
     key: '49074776-667ebd81d42a28579e0443e2e',
-    q: `${massege}`,
+    q: massege,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    page: page,
+    per_page: perPage,
   };
-
-  return axios.get(baseURL, { params }).then(res => res.data.hits);
+  const result = await axios.get(baseURL, { params });
+  
+  
+  return result.data;
 }
